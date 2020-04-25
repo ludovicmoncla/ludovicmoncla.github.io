198d9ccd0f63c43b091fedd4c3f6411f79b02431
lmoncla <moncla.ludovic@gmail.com>
2020-04-25 17:51:15 +0200

    update

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -10,11 +10,6 @@ var addressPoints = [
     -96.7968559
   ],
   [
-    "Leuven, Belgium",
-    50.879202,
-    4.7011675
-  ],
-  [
     "Lyon, France",
     45.7578137,
     4.8320114
@@ -38,10 +33,5 @@ var addressPoints = [
     "Chicago, IL, USA",
     41.8755616,
     -87.6244212
-  ],
-  [
-    "Redondo Beach, CA, USA",
-    33.8455911,
-    -118.3886766
   ]
 ];
